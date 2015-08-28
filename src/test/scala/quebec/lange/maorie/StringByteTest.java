cc61265121c785b46e08845d4e38d3feb84d9d50
Francois Langelier <f.langelier@gmail.com>
2015-08-28 16:28:31 -0400

    Maybe important stuff or not... just push everything out of my windows

---
+++ src/test/scala/quebec/lange/maorie/StringByteTest.java
@@ -0,0 +1,50 @@
+package quebec.lange.maorie;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringByteTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAsString() throws Exception {
+
+    }
+
+    @Test
+    public void testSet() throws Exception {
+
+    }
+
+    @Test
+    public void testSet_$eq() throws Exception {
+
+    }
+
+    @Test
+    public void testSet_$eq1() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAsStringOfHex() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAsByteArray() throws Exception {
+
+    }
+}
